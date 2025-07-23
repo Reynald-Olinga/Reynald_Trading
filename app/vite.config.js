af4309983ce5b2d73745e9488c11d078c0ebbe8a
Reynald <oreynald253@gmal.com>
2025-07-23 05:03:43 +0100

    fixed websocket

--- app/vite.config.js
+++ app/vite.config.js
@@ -6,17 +6,17 @@ import netlify from "@netlify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: "0.0.0.0",
-		proxy: {
-			"/api": {
-				target: 'https://reynaldtrading-production.up.railway.app', // URL de votre API
-				changeOrigin: true,
-				secure: false,
-				ws: true,
-				rewrite: path => path.replace(/^\/api/, '')
-			},
-		},
-	},
+	plugins: [react(), netlify()],
+	// server: {
+	// 	host: "0.0.0.0",
+	// 	proxy: {
+	// 		"/api": {
+	// 			target: 'https://reynaldtrading-production.up.railway.app', // URL de votre API
+	// 			changeOrigin: true,
+	// 			secure: false,
+	// 			ws: true,
+	// 			rewrite: path => path.replace(/^\/api/, '')
+	// 		},
+	// 	},
+	// },
 });
